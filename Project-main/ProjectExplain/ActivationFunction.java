850ba8bc5bd76b642d622ff6b1f56683b615ecfa
sb0611sb <sb0611sb@gmail.com>
2025-12-12 13:41:37 +0900

    Update ActivationFunction.java

--- Project-main/ProjectExplain/ActivationFunction.java
+++ Project-main/ProjectExplain/ActivationFunction.java
@@ -1,6 +1,6 @@
 public interface ActivationFunction {  
-    // public: 어디서나 사용할 수 있는 인터페이스. interface: 메서드 시그니처(형태)만 선언하는 타입.
-    // 신경망 레이어에서 사용하는 활성화 함수를 공통된 형태로 쓰기 위함
+    // interface: 메서드 형태만 선언
+    // 활성화 함수를 공통된 형태로 쓰기 위함
 
     /**
      * 활성화 함수 f(x)
@@ -12,9 +12,9 @@ public interface ActivationFunction {
      * 활성화 함수의 도함수 f'(y)
      */
     double derivative(double activatedValue);  
-    // 구현 클래스가 반드시 오버라이드해야 하는 추상 메서드
     // 역전파 때 필요
 
 }
-    // 이 인터페이스 덕분에 Sigmoid, ReLU 등 여러 함수로 쉽게 갈아끼울 수 있음(다형성)
+    // 이 인터페이스 덕분에 Sigmoid, ReLU 등 여러 함수로 쉽게 갈아끼울 수 있음
+
 
