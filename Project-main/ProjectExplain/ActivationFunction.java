84ce29826579bcf98b28c6420b0e05583ccbe4b5
sb0611sb <sb0611sb@gmail.com>
2025-12-12 13:42:14 +0900

    Update ActivationFunction.java

--- Project-main/ProjectExplain/ActivationFunction.java
+++ Project-main/ProjectExplain/ActivationFunction.java
@@ -6,7 +6,6 @@ public interface ActivationFunction {
      * 활성화 함수 f(x)
      */
     double activate(double x);          
-    // 인터페이스에서 선언된 메서드는 자동으로 public abstract
     //  가중합를 받아 f(x)를 계산하는 함수
     /**
      * 활성화 함수의 도함수 f'(y)
@@ -16,5 +15,6 @@ public interface ActivationFunction {
 
 }
     // 이 인터페이스 덕분에 Sigmoid, ReLU 등 여러 함수로 쉽게 갈아끼울 수 있음
+
 
 
